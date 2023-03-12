49b6ddbb4281b2fed9e7bde4a6a2f457dfe12673
Arthur Gonçalves Breguez <98524696+ArtBreguez@users.noreply.github.com>
2023-03-12 13:08:29 -0300

    código otimizado

--- app.py
+++ app.py
@@ -54,28 +54,22 @@ CHANNEL, CHAT_ID, BLAZE, API_HASH, API_ID, MODEL_PATH, CHANNEL_LINK = read_confi
 model = pickle.load(open(MODEL_PATH, 'rb'))
 model.epsilon = 0.03
 def getBlazeData():
-    global previous_payload
-    data = requests.get(BLAZE)
-    if data.status_code != 200:
-        raise Exception("Error getting data from blaze.com")
-    result = TotalPages(0, [])
-    result = json.loads(data.text)
-    payload = json.dumps(result["records"][:20])
-    if payload == previous_payload:
-        return None    
-    previous_payload = payload
-    global game_color
-    game_color.clear()
-    colors = ["red", "black", "white"]
-    encoder = LabelEncoder()
-    encoder.fit(colors)
-    for i, v in enumerate(result["records"]):
-        if i == 9:
-            break
-        color = v["color"]
-        game_color.append(encoder.transform([color])[0])
-    game_color.reverse()
-    return game_color
+    global previous_payload, game_color
+    try:
+        response = requests.get(BLAZE)
+        response.raise_for_status()
+        data = response.json()
+        payload = json.dumps(data["records"][:20])
+        if payload == previous_payload:
+            return None
+        previous_payload = payload
+        colors = ["red", "black", "white"]
+        encoder = LabelEncoder().fit(colors)
+        game_color = [encoder.transform([record["color"]])[0] for record in reversed(data["records"][:9])]
+        return game_color
+    except requests.exceptions.RequestException as e:
+        print(f"Error getting data from blaze.com: {e}")
+
 
 def predict(game_color):
     if np.random.rand() <= model.epsilon:
@@ -86,59 +80,57 @@ def predict(game_color):
     return action[0]
 
 def checkWin(game_color):
-
     global last_prediction
-    if last_prediction == '' or last_prediction is None or game_color is None:
+    if not last_prediction or not game_color:
         return
-    color_dict = {1:'red', 0:'black', 2:'white'}
+
+    color_dict = {1: 'red', 0: 'black', 2: 'white'}
     game_color_num = color_dict.get(game_color[-1])
+
     if game_color_num == last_prediction:
         log({"predicted": last_prediction, "result": game_color_num, "status": "win"})
     else:
         log({"predicted": last_prediction, "result": game_color_num, "status": "loss"})
 
+
 def calculate_win_loss_percentage():
-    local_tz = pytz.timezone('America/Sao_Paulo')
-    win_count = 0
-    loss_count = 0
-    total_count = 0
-    today = datetime.now(local_tz).strftime("%Y-%m-%d")
+    win_count = loss_count = total_count = 0
+    today = datetime.now(pytz.timezone('America/Sao_Paulo')).strftime("%Y-%m-%d")
     with open('logs/requests.log', 'r') as f:
         for line in f:
             match = re.search(r"^INFO:root:\[([\d-]+ [\d:]+)\] {'predicted': '(\w+)', 'result': '(\w+)', 'status': '(\w+)'}", line)
             if match:
                 log_date, predicted, result, status = match.groups()
-                # converte a data para o fuso horário local
-                log_date = pytz.timezone('America/New_York').localize(datetime.strptime(log_date, "%Y-%m-%d %H:%M:%S")).astimezone(local_tz).strftime("%Y-%m-%d")
+                log_date = datetime.fromisoformat(log_date).astimezone(pytz.timezone('America/Sao_Paulo')).strftime("%Y-%m-%d")
                 if log_date == today:
                     if predicted == result and status == 'win':
                         win_count += 1
                     elif predicted != result and status == 'loss':
                         loss_count += 1
                     total_count += 1
+
     win_percentage = round(win_count / total_count * 100, 2) if total_count > 0 else 0.0
     loss_percentage = round(loss_count / total_count * 100, 2) if total_count > 0 else 0.0
     return win_percentage, loss_percentage, total_count
 
+
 def send_message_to_telegram_channel(text):
-    message = ""
-    if text == 'black':
-        message = "A próxima jogada é ⚫"
-    elif text == 'red':
-        message = "A próxima jogada é 🔴"
-    elif text == 'white':
-        message = "A próxima jogada é ⚪"
-    elif text == 'help':
-        message = "👨🏼‍💻 Comandos disponíveis 🤖\n\n/start_stream - Inicia o stream de jogadas 🎰\n/stop_stream - Para o stream de jogadas 🛑\n/roll - Prediz a próxima jogada 🎲\n/statistics - Exibe as estatísticas de vitória/derrota 📈\n/last_plays - Exibe as últimas jogadas 🕹️"
-    elif text is None:
-        message = "👨🏼‍💻 Não há novas jogadas 🤖"
-    elif "Estatísticas" in text:
-        message = text
-    elif text == "last_plays":
+    messages = {
+        'black': "A próxima jogada é ⚫",
+        'red': "A próxima jogada é 🔴",
+        'white': "A próxima jogada é ⚪",
+        'help': "👨🏼‍💻 Comandos disponíveis 🤖\n\n/start_stream - Inicia o stream de jogadas 🎰\n/stop_stream - Para o stream de jogadas 🛑\n/roll - Prediz a próxima jogada 🎲\n/statistics - Exibe as estatísticas de vitória/derrota 📈\n/last_plays - Exibe as últimas jogadas 🕹️",
+        None: "👨🏼‍💻 Não há novas jogadas 🤖",
+    }
+
+    if text == "last_plays":
         getBlazeData()
         colors = game_color
         message = "👨🏼‍💻 Últimas jogadas 🤖\n\n" + convert_to_emoji(colors)
-                
+    elif "Estatísticas" in text:
+        message = text 
+    else:
+        message = messages.get(text, "")
 
     encoded_message = urllib.parse.quote(message)
     url = "https://api.telegram.org/bot" + CHANNEL + "/sendMessage?chat_id=" + CHAT_ID + "&text=" + encoded_message
@@ -149,23 +141,21 @@ def send_message_to_telegram_channel(text):
         logging.error("Error sending message to telegram channel: " + str(e))
         return
 
+
 def convert_to_emoji(game_color):
-    emoji_dict = {0: '⚫', 1: '🔴', 2: '⚪'}
-    emojis = [emoji_dict[color] for color in game_color]
-    return ''.join(emojis)
+    emojis = ['⚫', '🔴', '⚪']
+    return ''.join([emojis[color] for color in game_color])
 
 def log(message):
     try:
-        file = open("logs/requests.log", "a")
-    except:
-        logging.error("Failed to open file logs/requests.log")
-        return
-    
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_message = f"[{timestamp}] {message}"
-    logging.basicConfig(filename="logs/requests.log", level=logging.INFO)
-    logging.info(log_message)
-    file.close()
+        with open('logs/requests.log', 'a') as file:
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            log_message = f'[{timestamp}] {message}'
+            logging.basicConfig(filename='logs/requests.log', level=logging.INFO)
+            logging.info(log_message)
+    except Exception as e:
+        logging.error('Error writing to log file:', e)
+
 
 def getMachineGuess():
     data = getBlazeData()
@@ -173,79 +163,64 @@ def getMachineGuess():
         return send_message_to_telegram_channel(None)
     return send_message_to_telegram_channel(predict(data))
 
-def startStream(onlywhitelist=False):
+def startStream():
     global stream
     while stream:
         data = getBlazeData()
-        if data is None:
-            time.sleep(3)
-            continue
-        checkWin(data)
-        prediction = predict(data)
-        if onlywhitelist and prediction != 0:
-            time.sleep(3)
-            continue
-        send_message_to_telegram_channel(prediction)
+        if data:
+            checkWin(data)
+            send_message_to_telegram_channel(predict(data))
         time.sleep(3)
-    return
 
 def stopStream():
     global stream
     stream = False
-    return
 
-def startStreamInThread(onlywhitelist=False):
-    thread = threading.Thread(target=startStream, args=(onlywhitelist,))
-    thread.start()
-     
+def startStreamInThread():
+    threading.Thread(target=startStream).start()
 
 async def listenMessages():
-
     # Caminho para o arquivo com o número de telefone e o código de autenticação
     session_file = 'session_name.session'
 
-    # Inicializa o cliente
-    client = TelegramClient(session_file, API_ID, API_HASH)
-
-    # Conecta ao Telegram
-    await client.start()
-
-    # Encontra o ID do grupo
-    group = await client.get_entity(CHANNEL_LINK)
-    if isinstance(group, telethon.tl.types.Channel):
-        print('ID do canal:', group.id)
-        print('Nome do canal:', group.title)
-        await client(JoinChannelRequest(group.id))
-        print('Escutando mensagens do canal...')
-        while True:
-            messages = await client.get_messages(group.id, limit=1)
-            message = messages[0]
-            global stream
-            if message.message.strip().lower() == '/roll':
-                getMachineGuess()
-            if message.message.strip().lower() == '/start_stream':
-                if stream == True:
-                    return
-                stream = True
-                startStreamInThread()
-            if message.message.strip().lower() == '/stop_stream':
-                stopStream()
-            if message.message.strip().lower() == '/help':
-                send_message_to_telegram_channel('help')
-            if message.message.strip().lower() == '/statistics':
-                statistics = calculate_win_loss_percentage()
-                if statistics is None:
-                    send_message_to_telegram_channel("Não há estatísticas disponíveis")
-                else:
-                    send_message_to_telegram_channel(f"📈 Estatísticas 📈\n\nVitórias: {statistics[0]}%\nDerrotas: {statistics[1]}%\nTotal de jogadas: {statistics[2]}")
-            if message.message.strip().lower() == '/last_plays':
-                send_message_to_telegram_channel('last_plays')        
-            await asyncio.sleep(2)
-    else:
-        print(f'Canal "{CHANNEL_LINK}" não encontrado')
-        # Encerra a conexão
-        await client.disconnect()
-
+    # Inicializa o cliente e conecta ao Telegram
+    async with TelegramClient(session_file, API_ID, API_HASH) as client:
+        # Encontra o ID do grupo
+        try:
+            group = await client.get_entity(CHANNEL_LINK)
+            if isinstance(group, telethon.tl.types.Channel):
+                print('ID do canal:', group.id)
+                print('Nome do canal:', group.title)
+                await client(JoinChannelRequest(group.id))
+                print('Escutando mensagens do canal...')
+                while True:
+                    messages = await client.get_messages(group.id, limit=1)
+                    message = messages[0]
+                    global stream
+                    command = message.message.strip().lower()
+                    if command == '/roll':
+                        getMachineGuess()
+                    elif command == '/start_stream' and not stream:
+                        stream = True
+                        startStreamInThread()
+                    elif command == '/stop_stream' and stream:
+                        stopStream()
+                    elif command == '/help':
+                        send_message_to_telegram_channel('help')
+                    elif command == '/statistics':
+                        statistics = calculate_win_loss_percentage()
+                        if statistics is None:
+                            send_message_to_telegram_channel("Não há estatísticas disponíveis")
+                        else:
+                            send_message_to_telegram_channel(f"📈 Estatísticas 📈\n\nVitórias: {statistics[0]}%\nDerrotas: {statistics[1]}%\nTotal de jogadas: {statistics[2]}")
+                    elif command == '/last_plays':
+                        send_message_to_telegram_channel('last_plays')        
+                    await asyncio.sleep(2)
+            else:
+                print(f'Canal "{CHANNEL_LINK}" não encontrado')
+        except Exception as e:
+            print(f"Erro ao obter informações do canal: {e}")
+            return
     
 async def main():
     await listenMessages()
